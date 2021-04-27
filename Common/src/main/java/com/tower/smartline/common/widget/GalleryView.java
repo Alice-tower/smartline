f61b37d0b3bd8056e2b06ff6c737f529b1737a69
zpsong-tower <pingzisong2012@gmail.com>
2021-04-28 05:35:08 +0800

    图片选择器优化 规定构造Loader的PROJECTION，弃用图片路径字段等

--- Common/src/main/java/com/tower/smartline/common/widget/GalleryView.java
+++ Common/src/main/java/com/tower/smartline/common/widget/GalleryView.java
@@ -2,6 +2,7 @@ package com.tower.smartline.common.widget;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.AttributeSet;
@@ -24,7 +25,6 @@ import com.tower.smartline.common.widget.recycler.MyRecyclerAdapter;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -45,6 +45,10 @@ public class GalleryView extends RecyclerView {
 
     // 最小图片文件大小，小于10Kb过滤
     private static final int MIN_IMAGE_FILE_SIZE = 10 * 1024;
+
+    private static final int INDEX_ID = 0;
+
+    private static final int INDEX_SIZE = 1;
 
     private RecyclerAdapter mAdapter = new RecyclerAdapter();
 
@@ -69,6 +73,9 @@ public class GalleryView extends RecyclerView {
         init();
     }
 
+    /**
+     * RecyclerView初始化
+     */
     private void init() {
         setLayoutManager(new GridLayoutManager(getContext(), COLUMNS_NUM));
         mAdapter.setListener(new MyRecyclerAdapter.AdapterListener<Image>() {
@@ -88,16 +95,14 @@ public class GalleryView extends RecyclerView {
     }
 
     /**
-     * 初始化方法
+     * Loader初始化，为RecyclerView添加数据
      *
      * @param loaderManager Loader管理器
      * @param listener      选择变化监听器
-     * @return 返回LOADER_ID，可用于销毁Loader
      */
-    public int setup(LoaderManager loaderManager, SelectedChangeListener listener) {
+    public void setup(LoaderManager loaderManager, SelectedChangeListener listener) {
         mListener = listener;
         loaderManager.initLoader(LOADER_ID, null, mLoaderCallback);
-        return LOADER_ID;
     }
 
     /**
@@ -131,17 +136,17 @@ public class GalleryView extends RecyclerView {
     }
 
     /**
-     * 得到选中的图片的全部地址
+     * 得到选中的图片的全部Uri
      *
-     * @return 包含选中的图片全部地址的String数组
+     * @return 包含选中的图片全部Uri的String数组
      */
-    private String[] getSelectedPath() {
-        String[] paths = new String[mSelectedImages.size()];
+    private Uri[] getSelectedUri() {
+        Uri[] Uris = new Uri[mSelectedImages.size()];
         int index = 0;
         for (Image image : mSelectedImages) {
-            paths[index++] = image.mPath;
+            Uris[index++] = image.mUri;
         }
-        return paths;
+        return Uris;
     }
 
     /**
@@ -193,7 +198,6 @@ public class GalleryView extends RecyclerView {
 
         private CheckBox mSelected;
 
-
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             mPic = itemView.findViewById(R.id.im_image);
@@ -204,7 +208,7 @@ public class GalleryView extends RecyclerView {
         @Override
         protected void onBind(Image image) {
             Glide.with(getContext())
-                    .load(image.mPath) // 加载路径
+                    .load(image.mUri) // 加载资源
                     .diskCacheStrategy(DiskCacheStrategy.NONE) // 本地图片不使用缓存
                     .centerCrop() // 居中剪切
                     .placeholder(R.color.grey_200) // 加载时填充颜色
@@ -219,29 +223,27 @@ public class GalleryView extends RecyclerView {
      * 每张图片的数据结构
      */
     private static class Image {
-        // Id
-        private int mId;
-
-        // 路径
-        private String mPath;
-
-        // 创建日期
-        private long mDate;
+        // 资源标志
+        private Uri mUri;
 
         // 是否选中
         private boolean mIsSelect;
+
+        private Image(Uri mUri) {
+            this.mUri = mUri;
+        }
 
         @Override
         public boolean equals(Object o) {
             if (this == o) return true;
             if (o == null || getClass() != o.getClass()) return false;
             Image image = (Image) o;
-            return mPath != null ? mPath.equals(image.mPath) : image.mPath == null;
+            return mUri != null ? mUri.equals(image.mUri) : image.mUri == null;
         }
 
         @Override
         public int hashCode() {
-            return mPath != null ? mPath.hashCode() : 0;
+            return mUri != null ? mUri.hashCode() : 0;
         }
     }
 
@@ -251,8 +253,7 @@ public class GalleryView extends RecyclerView {
     private class LoaderCallback implements LoaderManager.LoaderCallbacks<Cursor> {
         private final String[] IMAGE_PROJECTION = new String[]{
                 MediaStore.Images.Media._ID,
-                MediaStore.Images.Media.DATA,
-                MediaStore.Images.Media.DATE_ADDED,
+                MediaStore.Images.Media.SIZE
         };
 
         /**
@@ -267,10 +268,10 @@ public class GalleryView extends RecyclerView {
         public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
             return new CursorLoader(getContext(),
                     MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    IMAGE_PROJECTION,
                     null,
                     null,
-                    null,
-                    IMAGE_PROJECTION[2] + " DESC"); // 按时间倒序查询
+                    MediaStore.Images.Media.DATE_ADDED + " DESC"); // 按时间倒序查询
         }
 
         /**
@@ -289,26 +290,20 @@ public class GalleryView extends RecyclerView {
                     data.moveToFirst();
 
                     // 得到对应的列的Index坐标
-                    int indexId = data.getColumnIndexOrThrow(IMAGE_PROJECTION[0]);
-                    int indexPath = data.getColumnIndexOrThrow(IMAGE_PROJECTION[1]);
-                    int indexDate = data.getColumnIndexOrThrow(IMAGE_PROJECTION[2]);
+                    int indexId = data.getColumnIndexOrThrow(IMAGE_PROJECTION[INDEX_ID]);
+                    int indexSize = data.getColumnIndexOrThrow(IMAGE_PROJECTION[INDEX_SIZE]);
 
                     // 循环读取，直到没有下一条数据
                     do {
-                        int id = data.getInt(indexId);
-                        String path = data.getString(indexPath);
-                        long dateTime = data.getLong(indexDate);
-                        File file = new File(path);
-                        if (!file.exists() || file.length() < MIN_IMAGE_FILE_SIZE) {
-                            // 如果没有图片，或者图片大小太小，则跳过
+                        if (data.getLong(indexSize) < MIN_IMAGE_FILE_SIZE) {
+                            // 如果图片大小太小，则跳过
                             continue;
                         }
+                        String id = data.getString(indexId);
+                        Uri uri = Uri.withAppendedPath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
 
                         // 添加一条新的数据
-                        Image image = new Image();
-                        image.mId = id;
-                        image.mPath = path;
-                        image.mDate = dateTime;
+                        Image image = new Image(uri);
                         images.add(image);
                     } while (data.moveToNext());
                 }
